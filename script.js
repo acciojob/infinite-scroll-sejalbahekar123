4f2bd7488e2860a1151f9ed6df98c39f9f31be53
sejalbahekar123 <sejalbahekar123@gmail.com>
2026-02-19 18:15:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-//your code here!
-
+const list = document.getElementById("infi-list");
+function addItems(count) {
+  for (let i = 0; i < count; i++) {
+    const li = document.createElement("li");
+    const itemNumber = list.children.length + 1;
+    li.textContent = `Item ${itemNumber}`;
+    list.appendChild(li);
+  }
+}
+addItems(10);
+list.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight, scrollHeight } = list;
+  if (scrollTop + clientHeight >= scrollHeight - 1) {
+    addItems(2);
+  }
+});
